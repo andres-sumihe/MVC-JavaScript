e9dd1d91b4b55fd238eee2af7369b5299468beae
andres-sumihe <672018136@student.uksw.edu>
2020-09-23 23:42:02 +0700

    Change Code Structure, migrate to ES6

--- index.js
+++ index.js
@@ -1,8 +1,58 @@
+import App from './lib/App.js';
+import Router from './lib/Router.js';
+import API from './lib/API.js';
 
-function main(){
-    console.log("IN MAIN");
-    var model = new Model();
-    var controller = new Controller(model);
-    var view = new View(controller);
-}
+const app = new App('#app');
+const router = new Router(app);
 
+const dogTemplate = (dog) => `
+<section class="dog-listing">
+  <a href="#/dogs/${dog.id}">
+    <h3 class="name">${dog.name}</h3>
+    <section>
+      <figure>
+        <img src="${dog.url}" alt="${dog.name}" />
+        <figcaption>${dog.description}</figcaption>
+      </figure>
+      <p>${dog.description}</p>
+    </section>
+  </a>
+</section>
+`;
+
+app.addComponent({
+    name: 'dogs',
+    model: {
+        dogs: []
+    },
+
+    view(model) {
+        return `
+      <ul class="dogs">
+        ${model.dogs.map(item => `<li>${dogTemplate(item)}<li>`).join('')}
+      </ul>
+    `;
+    },
+
+    async controller(model){
+        const dogs = await API.getDogs();
+        model.dogs = dogs;
+    }
+})
+app.addComponent({
+    name: 'dog',
+    model: {
+        dog: []
+    },
+
+    view(model) {
+        return dogTemplate(model.dog);
+    },
+
+    async controller(model){
+        const dog = await API.getDog(router.params[1]);
+        model.dog = dog;
+    }
+})
+router.addRoute('dogs', '^#/dogs$');
+router.addRoute('dog', '^#/dogs/([0-9]+)$');
