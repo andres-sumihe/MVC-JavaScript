13a82be158cf4962d7185e26431357bad4234529
andres-sumihe <672018136@student.uksw.edu>
2020-09-27 00:35:12 +0700

    Add imitWord function

--- View/View.js
+++ View/View.js
@@ -2,14 +2,31 @@
 class View {
     constructor() {
     }
+    limitWords(textToLimit, wordLimit){
+        var finalText = "";
+        var text2 = textToLimit.replace(/\s+/g, ' ');
+        var text3 = text2.split(' ');
+        var numberOfWords = text3.length;
+        var i=0;
+
+        if(numberOfWords > wordLimit){
+            for(i=0; i< wordLimit; i++)
+                finalText = finalText+" "+ text3[i]+" ";
+
+            return finalText+"…";
+        }
+        else return textToLimit;
+    }
     newsCard = (model) => {
        return `
        <div class="col-md-3 custom-row">
             <div class="card">
-                <img src="${model.urlToImage}" class="img-fluid"/>
+                <div class="img-box">
+                    <img src="${model.urlToImage}" class="img-fluid"/>
+                </div>
                 <div class="card-body">
                     <h6>${model.title}</h6>
-                    <p class="text-justify">${model.description}</p>
+                    <p class="text-justify">${this.limitWords(model.description, 20)}</p>
                     <button type="button" class="btn btn-primary w-50" 
                         onClick="window.location.href='${model.url}'"
                     >Read More</button>
